b647d7c83b5f6bdb756669f6ae3f228c9fe0ac09
TheHeroCraft1579 <maximilian.doetsch@outlook.de>
2020-09-13 11:57:49 +0200

    Added web server for serving rss files

--- bot.js
+++ bot.js
@@ -15,9 +15,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/', function(req, res){
+  res.sendFile('rss/feed.rss');
+}); 
 
 app.listen(process.env.PORT || 8080);
 
